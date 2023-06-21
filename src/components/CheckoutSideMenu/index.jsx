4328def2c0bb01630ff1011e9d545b8601e89573
Andres Badillo <r.andres.badillo@gmail.com>
2023-06-21 18:48:24 -0500

    Clase 19 - Eliminando productos del carrito

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -7,6 +7,11 @@ import './styles.css';
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext);
 
+    const handleDelete =(id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id);
+        context.setCartProducts(filteredProducts);
+    }
+
     return (
         <aside 
             className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} scrollable-cards checkout-side-menu flex-col fixed right-0 border border-black bg-white rounded-lg`}
@@ -23,9 +28,11 @@ const CheckoutSideMenu = () => {
                         <div className='my-3'>
                             <OrderCard 
                                 key={product.id}
+                                id={product.id}
                                 title={product.title}
                                 imageUrl={product.images}
                                 price={product.price}
+                                handleDelete={handleDelete}
                             />
                         </div>
                     ))
