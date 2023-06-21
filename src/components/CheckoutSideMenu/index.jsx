1b5c0f42b1d3dd36265f71ad69f1d18877ef5775
Andres Badillo <r.andres.badillo@gmail.com>
2023-06-21 18:07:56 -0500

    Clase 17 - Se creo el componente OrderCard y se agregaron productos seleccionados

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -1,20 +1,36 @@
 import { useContext } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 import { ShoppingCartContext } from '../../context';
+import { OrderCard } from '../OrderCard';
 import './styles.css';
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext);
+    console.log('CART: ', context.cartProducts);
 
     return (
         <aside 
-            className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black bg-white rounded-lg`}
+            className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} scrollable-cards checkout-side-menu flex-col fixed right-0 border border-black bg-white rounded-lg`}
         >
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>My Order</h2>
                 <div onClick={() => context.closeCheckoutSideMenu()}>
                     <XMarkIcon className='h-6 w-6 text-black cursor-pointer'/>
                 </div>
+            </div>
+            <div className='px-6'>
+                {
+                    context.cartProducts.map(product => (
+                        <div className='my-3'>
+                            <OrderCard 
+                                key={product.id}
+                                title={product.title}
+                                imageUrl={product.images}
+                                price={product.price}
+                            />
+                        </div>
+                    ))
+                }
             </div>
         </aside>
     )
