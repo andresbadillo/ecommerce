1779ce0e06e910fada670d6e35831ed1aec055f7
Andres Badillo <r.andres.badillo@gmail.com>
2023-06-21 18:36:14 -0500

    Clase 18 - Se añadieron validaciones al agregar productos al carrito

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../context';
-import { PlusIcon } from '@heroicons/react/24/solid';
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid';
 
 const Card = (data) => {
     const context = useContext(ShoppingCartContext);
@@ -18,6 +18,28 @@ const Card = (data) => {
         context.closeProductDetail();
     }
 
+    const renderIcon = (id) => {
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0;
+        if (isInCart) {
+            return (
+                <div 
+                    className='absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1'
+                >
+                    <CheckIcon className='h-6 w-6 text-white'/>
+                </div>
+            );
+        } else {
+            return (
+                <div 
+                    className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
+                    onClick={(event) => addProductToCart(event, data.data)}
+                >
+                    <PlusIcon className='h-6 w-6 text-black'/>
+                </div>
+            );
+        }
+    }
+
     return (
         <div 
             className='bg-white cursor-pointer w-56 h-60 rounded-lg'
@@ -32,12 +54,7 @@ const Card = (data) => {
                     src={data.data.images[0]} 
                     alt={data.data.title}
                 ></img>
-                <div 
-                    className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-                    onClick={(event) => addProductToCart(event, data.data)}
-                >
-                    <PlusIcon className='h-6 w-6 text-black'/>
-                </div>
+                {renderIcon(data.data.id)}
             </figure>
             <p className='flex justify-between'>
                 <span className='text-sm font-light'>{data.data.title}</span>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,8 +14,7 @@ function Home() {
 
   return (
     <Layout>
-      Home
-      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+      <div className='grid gap-4 grid-cols-3 w-full max-w-screen-md'>
         {
           items?.map(item => (
             <Card key={item.id} data={item} />
