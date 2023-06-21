2cbc3ef2f33afa5b394ec2084a8411f3989a3ab5
Andres Badillo <r.andres.badillo@gmail.com>
2023-06-21 15:05:13 -0500

    Clase 13 - Se agrego comportamiento de apertura y cierre al productdetail

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid';
-
+import { ShoppingCartContext } from '../../context';
 import './styles.css'
 
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext);
+
     return (
-        <aside className='product-detail flex flex-col fixed right-0 border border-black bg-white rounded-lg'>
+        <aside 
+            className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black bg-white rounded-lg`}
+        >
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div>
+                <div onClick={() => context.closeProductDetail()}>
                     <XMarkIcon className='h-6 w-6 text-black'/>
                 </div>
             </div>
