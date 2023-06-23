dc0c74391b0ba3178706292ab39d28421d36b026
Andres Badillo <r.andres.badillo@gmail.com>
2023-06-23 14:39:28 -0500

    Clase 26 - Se añadio el buscador de productos

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const ShoppingCartContext =createContext();
 
@@ -25,6 +25,18 @@ export const ShoppingCartProvider = ({children}) => {
     // ShoppingCart - Order
     const [order, setOrder] = useState([]);
 
+    // Get Products
+    const [items, setItems] = useState(null);
+
+    // Get Products by title
+    const [searchByTitle, setSearchByTitle] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+          .then(response => response.json())
+          .then(data => setItems(data))
+      }, []);
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
@@ -41,6 +53,10 @@ export const ShoppingCartProvider = ({children}) => {
             isCheckoutSideMenuOpen,
             order,
             setOrder,
+            items,
+            setItems,
+            searchByTitle,
+            setSearchByTitle,
         }}>
             {children}
         </ShoppingCartContext.Provider>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../context';
 import { Layout } from '../../components/Layout';
 import { Card } from '../../components/Card';
 import { ProductDetail } from '../../components/ProductDetail';
 
 function Home() {
-  const [items, setItems] = useState(null);
-
-  useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then(response => response.json())
-      .then(data => setItems(data))
-  }, []);
+  const context = useContext(ShoppingCartContext);
 
   return (
     <Layout>
+      <div className='flex relative w-60 justify-center items-center mb-4'>
+        <h1 className='font-medium text-xl'>Exclusive Products</h1>
+      </div>
+      <input 
+        className='w-80 rounded-lg border border-black p-2 mb-4'
+        type='text' 
+        placeholder='Search products'
+        onChange={(event) => context.setSearchByTitle(event.target.value)}
+      />
       <div className='grid gap-4 grid-cols-3 w-full max-w-screen-md'>
         {
-          items?.map(item => (
+          context.items?.map(item => (
             <Card key={item.id} data={item} />
           ))
         }
